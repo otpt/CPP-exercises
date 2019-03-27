01fd34f82591016ee7b72cfb64aba415b96aa04a
Valery Baiburin <valery@ortech.co.jp>
2019-03-27 21:31:10 +0900

    Add solutions to metaprogramming problems

---
+++ solutions/metaprogramming_1.cpp
@@ -0,0 +1,14 @@
+template<int N>
+struct Fib {
+    static int const value = Fib<N - 1>::value + Fib<N - 2>::value;
+};
+
+template<>
+struct Fib<0> {
+    static int const value = 0;
+};
+
+template<>
+struct Fib<1> {
+    static int const value = 1;
+};
